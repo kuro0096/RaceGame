e8392cac0371a5ac97a1cd6822963a35aac90a8c
nnn0617 <49850358+nnn0617@users.noreply.github.com>
2019-12-03 18:07:05 +0900

    sound

---
+++ RaceGame/Assets/Script/sound/SoundVolume.cs
@@ -0,0 +1,16 @@
+﻿// 音量クラス
+public class SoundVolume
+{
+   public float BGM = 1.0f;
+   public float Voice = 1.0f;
+   public float SE = 1.0f;
+   public bool Mute = false;
+
+   public void Init()
+   {
+      BGM = 1.0f;
+      Voice = 1.0f;
+      SE = 1.0f;
+      Mute = false;
+   }
+}
