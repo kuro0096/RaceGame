56e6ac67e70cf0de12f0754a89cede250ee8c9c1
kuro0096 <kc8520993@gmail.com>
2019-12-20 17:37:43 +0900

    a

--- RaceGame/Assets/Script/player/PlayerCtr.cs
+++ RaceGame/Assets/Script/player/PlayerCtr.cs
@@ -4,60 +4,60 @@ using UnityEngine;
 
 public class PlayerCtr : MonoBehaviour
 {
-    [SerializeField] float m_speed = 0.5f;
-    [SerializeField] float m_accel = 0.1f;
+    //[SerializeField] float m_speed = 0.5f;
+    //[SerializeField] float m_accel = 0.1f;
   
-    // Start is called before the first frame update
-    void Start()
-    {
+    //// Start is called before the first frame update
+    //void Start()
+    //{
        
-    }
+    //}
 
-    // Update is called once per frame
-    void Update()
-    {
-        AccelUpdate();
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    AccelUpdate();
 
-        float LStickH = Input.GetAxis("LStick_H");
-        float LStickV = Input.GetAxis("LStick_V");
-        //  左右移動
-        if(LStickH >= 0.1f)
-        {
-            this.transform.Translate(LStickH * m_accel, 0.0f, 0.0f);
-        }
-        else if (LStickH <= -0.1f)
-        {
-            this.transform.Translate(LStickH * m_accel, 0.0f, 0.0f);
-        }
-        // 上下移動
-        if (LStickV >= 0.1f)
-        {
-            this.transform.Translate(0.0f, 0.0f, m_speed * m_accel);
-        }
-        else if (LStickV <= -0.1f)
-        {
-            this.transform.Translate(0.0f, 0.0f, -m_speed * m_accel);
-        }
-    }
+    //    float LStickH = Input.GetAxis("LStick_H");
+    //    float LStickV = Input.GetAxis("LStick_V");
+    //    //  左右移動
+    //    if(LStickH >= 0.1f)
+    //    {
+    //        this.transform.Translate(LStickH * m_accel, 0.0f, 0.0f);
+    //    }
+    //    else if (LStickH <= -0.1f)
+    //    {
+    //        this.transform.Translate(LStickH * m_accel, 0.0f, 0.0f);
+    //    }
+    //    // 上下移動
+    //    if (LStickV >= 0.1f)
+    //    {
+    //        this.transform.Translate(0.0f, 0.0f, m_speed * m_accel);
+    //    }
+    //    else if (LStickV <= -0.1f)
+    //    {
+    //        this.transform.Translate(0.0f, 0.0f, -m_speed * m_accel);
+    //    }
+    //}
 
-    private void AccelUpdate()
-    {
-        if(m_accel >= 0.300f)
-        {
-            m_accel = 0.3f;
-        }
-        if (m_accel <= 0.100f)
-        {
-            m_accel = 0.1f;
-        }
+    //private void AccelUpdate()
+    //{
+    //    if(m_accel >= 0.300f)
+    //    {
+    //        m_accel = 0.3f;
+    //    }
+    //    if (m_accel <= 0.100f)
+    //    {
+    //        m_accel = 0.1f;
+    //    }
 
-        if (Input.GetButton("Accel") == true)
-        {
-            m_accel += 0.001f;
-        }
-        if (Input.GetButton("Accel") == false)
-        {
-            m_accel -= 0.001f;
-        }
-    }
+    //    if (Input.GetButton("Accel") == true)
+    //    {
+    //        m_accel += 0.001f;
+    //    }
+    //    if (Input.GetButton("Accel") == false)
+    //    {
+    //        m_accel -= 0.001f;
+    //    }
+    //}
 }
---
+++ RaceGame/Assets/Script/player/Test.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Test : MonoBehaviour
+{
+    // インプットの登録と破棄
+    PlayerAct input;
+    void Awake() => input = new PlayerAct();
+    void OnDisable() => input.Disable();
+
+    // インプットの有効・無効化
+    void OnDestroy() => input.Disable();
+    void OnEnable() => input.Enable();
+
+
+    void Update()
+    {
+        // スティックの移動を取得して動かす
+        var velocity = input.ActionTest.Move.ReadValue<Vector2>();
+        Debug.Log($"Move[{velocity}]");
+
+        // ジャンプボタンが押されているか判定して動かす
+        var isJumpButtonPressed = (input.ActionTest.Jump.ReadValue<float>() >= InputSystem.settings.defaultButtonPressPoint);
+        if (isJumpButtonPressed)
+        {
+            this.transform.Translate(0.0f, 1.0f, 0.0f);
+        }
+    }
+}
