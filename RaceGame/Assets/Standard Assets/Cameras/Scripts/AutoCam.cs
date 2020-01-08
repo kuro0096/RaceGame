244b1f9674a674fa83f32677066729a42b6bda1e
daiiiiiiiiiiiii <50856200+daiiiiiiiiiiiii@users.noreply.github.com>
2020-01-08 14:59:58 +0900

    a

--- RaceGame/Assets/Standard Assets/Cameras/Scripts/AutoCam.cs
+++ RaceGame/Assets/Standard Assets/Cameras/Scripts/AutoCam.cs
@@ -9,12 +9,12 @@ namespace UnityStandardAssets.Cameras
     [ExecuteInEditMode]
     public class AutoCam : PivotBasedCameraRig
     {
-        [SerializeField] private float m_MoveSpeed = 3; // How fast the rig will move to keep up with target's position
-        [SerializeField] private float m_TurnSpeed = 1; // How fast the rig will turn to keep up with target's rotation
-        [SerializeField] private float m_RollSpeed = 0.2f;// How fast the rig will roll (around Z axis) to match target's roll.
-        [SerializeField] private bool m_FollowVelocity = false;// Whether the rig will rotate in the direction of the target's velocity.
-        [SerializeField] private bool m_FollowTilt = true; // Whether the rig will tilt (around X axis) with the target.
-        [SerializeField] private float m_SpinTurnLimit = 90;// The threshold beyond which the camera stops following the target's rotation. (used in situations where a car spins out, for example)
+        [SerializeField] private float m_MoveSpeed = 3; // cameraのボーンが動くスピード
+        [SerializeField] private float m_TurnSpeed = 1; // 車がターンするときのcameraの速さ
+        [SerializeField] private float m_RollSpeed = 0.2f;// cameraの時点の速さ
+        [SerializeField] private bool m_FollowVelocity = true;// Whether the rig will rotate in the direction of the target's velocity.
+        [SerializeField] private bool m_FollowTilt = false; // Whether the rig will tilt (around X axis) with the target.
+        [SerializeField] private float m_SpinTurnLimit = 90;// ターゲットの回転に続いてカメラが停止するしきい値 (used in situations where a car spins out, for example)
         [SerializeField] private float m_TargetVelocityLowerLimit = 4f;// the minimum velocity above which the camera turns towards the object's velocity. Below this we use the object's forward direction.
         [SerializeField] private float m_SmoothTurnTime = 0.2f; // the smoothing for the camera's rotation
 
